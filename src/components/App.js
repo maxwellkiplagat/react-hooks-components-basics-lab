8544f1c06e403e531d7d648197e813f4106f1fcd
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-04-10 11:53:40 +0300

    completed assignemt

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,19 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return(
+  <div id='about' > <h2>my name is maxwell kiplagat am learning react js for front end development</h2></div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
